956d549d066005f744dbda73130dbd289c7f3abe
faisal muaris <faisal.muaris.fm@gmail.com>
2024-09-27 14:00:05 +0700

    Menampilkan semua data mahasiswa dari database

--- controller.js
+++ controller.js
@@ -6,3 +6,11 @@ let connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("Aplikasi Rest Api ku berjalan", res);
 };
+
+// menampilkan semua data mahasiswa
+exports.tampilMahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", (err, rows, fields) => {
+    if (err) throw err;
+    response.ok(rows, res);
+  });
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   let jsonKu = require("./controller");
 
   app.route("/").get(jsonKu.index);
+  app.route("/mahasiswa").get(jsonKu.tampilMahasiswa);
 };
