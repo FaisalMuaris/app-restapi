36bb38e99b20727e9af5ed4b207b8db28018dd48
faisal muaris <faisal.muaris.fm@gmail.com>
2024-09-27 13:29:36 +0700

    second commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const app = express();
+
+// parse apllication/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.listen(port, () => {
+  console.log(`Server started on port`);
+});
